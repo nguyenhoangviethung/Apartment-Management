5fbb0ee98252daa8a8bfbe0847b80ebc53714f59
dung1311 <109650915+dung1311@users.noreply.github.com>
2024-10-04 03:04:55 +0700

    finish feature login-admin-require

--- backend/api/auth/routes.py
+++ backend/api/auth/routes.py
@@ -2,7 +2,7 @@ from api.auth import auth_bp
 from api.extensions import db
 from sqlalchemy import select
 from api.models.models import *
-from flask import g, redirect, url_for, request, session
+from flask import g, redirect, url_for, session, abort, request
 from functools import wraps
 
 
@@ -11,54 +11,54 @@ def login_require(f):
     @wraps(f)
     def wrap(*args, **kwargs):
         if g.get('user') is None:
-            return redirect(url_for(endpoint='test'))
+            # unauthorized
+            return redirect(url_for(endpoint='test'), code=401)
         return f(*args, **kwargs)
     
     return wrap
 
-def admin_require(f):
+def admin_required(f):
     @wraps(f)
     def wrap(*args, **kwargs):
-        if g.get('role') != 'admin':
-            return "KHONG PHAI ADMIN"
+        if not g.get('user') or g.get('user').user_role != 'admin':
+            # forbidden
+            return abort(403)  
         return f(*args, **kwargs)
-    
     return wrap
 
-@auth_bp.route('/login', methods=('GET', 'POST'))
-def login():
-    if request.method == 'POST':
-        name, password, role = request.form['user_name'], request.form['password_hash'], request.form['role']
-        user = db.session.query(Users, Users.user_id, Users.user_role).filter_by(user_name=name, password_hash=password).one_or_none()
-        if user is None:
-            return "CO GI DO DEO DUNG"
-        session.clear()
-        session['user_id'] = user.__getattr__('user_id')
-        session['role'] = user.__getattr__('user_role')
-        return "OKE ROI NHE"
-
 @auth_bp.before_app_request
-def check():
+def check_attribute():
     user_id = session.get('user_id')
-    role = session.get('role')
-    if user_id is None:
-        g.user = None
+    if user_id:
+        g.user = db.session.query(Users).filter_by(user_id=user_id).one_or_none()
     else:
-        g.user = db.session.query(Users).filter_by(user_id=user_id)
-        g.role = db.session.query(Users.user_role).filter_by(user_id=user_id)
+        g.user = None
+
+@auth_bp.post('/add')
+def add():
+    name, password = request.form['user_name'], request.form['password']
+    user = db.session.query(Users).filter_by(user_name=name, password_hash=password).one_or_none()
+    if user is None:
+        return "somthing went wrong"
+    
+    session.clear()
+    session.setdefault('user_id', user.user_id)
+    session.setdefault('user_role', user.user_role)
+
+    return "PASS LOGIN"
+
+@auth_bp.route('/admin')
+@admin_required
+def admin():
+    return "admin"
 
 @auth_bp.route('/logout')
 def logout():
     session.clear()
+    return "clear session"
 
-    return "da xoa session"
-
-@auth_bp.route('/test-login')
+@auth_bp.route('/login')
 @login_require
-def test_login():
-    return "MAY DA DANG NHAP ROI NHE"
+def login():
+    return "access success"
 
-@auth_bp.route('/test-admin')
-@admin_require
-def test_admin():
-    return "MAY CHINH LA ADMIN"

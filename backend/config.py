f2b357a1a28cb67a5585b446e87eb55ada43da55
dung1311 <tiendung13112004@gmail.com>
2024-10-04 22:40:51 +0700

    update config

--- backend/config.py
+++ backend/config.py
@@ -2,3 +2,10 @@ class Config:
     SECRET_KEY = 'dev'
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:Dungdepzai1!@localhost/cnpm'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 465
+    MAIL_USERNAME = 'nguyenhoangviethung@gmail.com'
+    MAIL_PASSWORD = 'bfrq ebrv avbi jzqx'
+    MAIL_USE_SSL = True
+    MAIL_USE_TLS = False

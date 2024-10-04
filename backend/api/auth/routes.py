0c5bd58d6b0fddaa9d35199a547c820076e51ecf
nguyenhoangviethung <nguyenhoangviethung@gmail.com>
2024-10-04 22:32:33 +0700

    Complete register feature with confirm email

--- backend/api/auth/routes.py
+++ backend/api/auth/routes.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify, url_for,current_app
+from flask_jwt_extended import create_access_token
+from flask_mail import Mail, Message
+import jwt, datetime
 from ..models.models import db, Users
 import uuid
 from werkzeug.security import generate_password_hash
@@ -21,16 +24,57 @@ def register():
     if check:
         return jsonify({"error": "user_email already exists"}), 400
     
-    new_user = Users(
-        user_id = uuid.uuid4().bytes,
-        username = user_name,
-        password_hash = generate_password_hash(password),
-        user_email = email
+    token = jwt.encode(
+        payload = {
+            'email': email,
+            'user_name': user_name,
+            'password': password,
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=1)
+        },
+        key = current_app.config['SECRET_KEY'],
+        algorithm='HS256'
     )
 
-    db.session.add(new_user)
-    db.session.commit()
+    confirm_link = url_for('auth.confirm_email', token=token, _external=True)
+    try:
+        mail = Mail(current_app)
+        msg = Message(
+            subject = 'confirm_email',
+            body = f'''Please confirm your email address!
+            Click here to confirm your email address
+            {confirm_link}
+            ''',
+            sender = 'nguyenhoangviethung@gmail.com',
+            recipients = [email]
+        )
+        mail.send(msg)
+        return jsonify({'message': 'send confirm-email success'})
+    except Exception as e:
+        return jsonify({'message': 'send confirm-email failure', 'error': str(e)})
 
-    return jsonify({"message": "user registration successful"}), 200
+@auth.route('/confirm_email/<token>', methods=['POST','GET'])
+def confirm_email(token):
+    try:
+        data = jwt.decode(token, current_app.config['SECRET_KEY'], algorithms='HS256')
 
+        email = data.get('email')
+        user_name = data.get('user_name')
+        password = data.get('password')
+
+        new_user = Users(
+            user_id = uuid.uuid4().bytes,
+            username = user_name,
+            password_hash = generate_password_hash(password),
+            user_email = email
+        )
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        return jsonify({"message": "user registration successful"}), 200
+    except jwt.InvalidTokenError:
+        return jsonify({'message': 'Token invalid!'}), 400
+    except jwt.ExpiredSignatureError:
+        return jsonify({"message": "Token expired!"}), 400
+    
     

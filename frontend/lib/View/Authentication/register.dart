615ba198bdd941d50925f9bd92b7c70e6a203101
jien0404 <146214487+jien0404@users.noreply.github.com>
2024-10-06 22:43:50 +0700

    fix register
    add appbar register

--- frontend/lib/View/Authentication/register.dart
+++ frontend/lib/View/Authentication/register.dart
@@ -25,134 +25,163 @@ class _RegisterState extends State<Register> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: const Text(
+            'Register',
+            style: TextStyle(
+              fontSize: 25,
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          leading: Builder(
+              builder: (context) {
+                return IconButton(
+                  icon: const Icon(
+                    Icons.arrow_back,
+                    size: 30,
+                    color: Colors.white,
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const Login()),
+                    );
+                  },
+                );
+              }
+          ),
+        ),
         body: GestureDetector(
           onTap: () {
             FocusScope.of(context).unfocus(); // Ẩn bàn phím khi nhấn ra ngoài
           },
           child: Stack(
             children: [
-              Center(
-                child: SingleChildScrollView(
-                  padding: const EdgeInsets.all(16),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      Icon(Icons.account_circle, size: 50, color: Colors.grey[700]),
-                      const SizedBox(height: 8),
-                      const Text(
-                        'Register',
-                        style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.w600,
-                          color: Colors.blue,
+              SingleChildScrollView(
+                padding: const EdgeInsets.all(16),
+                child: Column(
+                  // mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    const SizedBox(
+                      height: 50,
+                    ),
+                    Icon(Icons.account_circle, size: 50, color: Colors.grey[700]),
+                    const SizedBox(height: 8),
+                    const Text(
+                      'Register',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.blue,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    const SizedBox(height: 28),
+                    // Username field
+                    TextFormField(
+                      controller: _username,
+                      decoration: InputDecoration(
+                        labelText: 'USERNAME',
+                        prefixIcon: Icon(Icons.person),
+                        filled: true,
+                        fillColor: Colors.white,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                          borderSide: BorderSide(color: Colors.black, width: 2),
                         ),
+                      ),
+                    ),
+                    const SizedBox(height: 28),
+                    // Email field
+                    TextFormField(
+                      controller: _email,
+                      decoration: InputDecoration(
+                        labelText: 'EMAIL',
+                        prefixIcon: const Icon(Icons.email),
+                        filled: true,
+                        fillColor: Colors.white,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                          borderSide: const BorderSide(color: Colors.black, width: 2),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 28),
+                    // Password field
+                    TextFormField(
+                      controller: _password,
+                      obscureText: !_showpass,
+                      decoration: InputDecoration(
+                        labelText: 'PASSWORD',
+                        prefixIcon: const Icon(Icons.lock),
+                        filled: true,
+                        fillColor: Colors.white,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                          borderSide: const BorderSide(color: Colors.black, width: 2),
+                        ),
+                        suffixIcon: IconButton(
+                          icon: Icon(
+                            _showpass ? Icons.visibility : Icons.visibility_off,
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              _showpass = !_showpass;
+                            });
+                          },
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 28),
+                    // Confirm Password field
+                    TextFormField(
+                      controller: _confirmpass,
+                      obscureText: !_showconfirmpass,
+                      decoration: InputDecoration(
+                        labelText: 'CONFIRM PASSWORD',
+                        prefixIcon: const Icon(Icons.lock),
+                        filled: true,
+                        fillColor: Colors.white,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                          borderSide: const BorderSide(color: Colors.black, width: 2),
+                        ),
+                        suffixIcon: IconButton(
+                          icon: Icon(
+                            _showconfirmpass ? Icons.visibility : Icons.visibility_off,
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              _showconfirmpass = !_showconfirmpass;
+                            });
+                          },
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 55),
+                    // Sign up button
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.blue,
+                        padding: const EdgeInsets.symmetric(vertical: 15),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                      ),
+                      onPressed: () {},
+                      child: const Text(
+                        'SIGN UP',
+                        style: TextStyle(
+                            fontSize: 24,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold),
                         textAlign: TextAlign.center,
                       ),
-                      const SizedBox(height: 28),
-                      // Username field
-                      TextFormField(
-                        controller: _username,
-                        decoration: InputDecoration(
-                          labelText: 'USERNAME',
-                          prefixIcon: Icon(Icons.person),
-                          filled: true,
-                          fillColor: Colors.white,
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(8),
-                            borderSide: BorderSide(color: Colors.black, width: 2),
-                          ),
-                        ),
-                      ),
-                      const SizedBox(height: 28),
-                      // Email field
-                      TextFormField(
-                        controller: _email,
-                        decoration: InputDecoration(
-                          labelText: 'EMAIL',
-                          prefixIcon: const Icon(Icons.email),
-                          filled: true,
-                          fillColor: Colors.white,
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(8),
-                            borderSide: const BorderSide(color: Colors.black, width: 2),
-                          ),
-                        ),
-                      ),
-                      const SizedBox(height: 28),
-                      // Password field
-                      TextFormField(
-                        controller: _password,
-                        obscureText: !_showpass,
-                        decoration: InputDecoration(
-                          labelText: 'PASSWORD',
-                          prefixIcon: const Icon(Icons.lock),
-                          filled: true,
-                          fillColor: Colors.white,
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(8),
-                            borderSide: const BorderSide(color: Colors.black, width: 2),
-                          ),
-                          suffixIcon: IconButton(
-                            icon: Icon(
-                              _showpass ? Icons.visibility : Icons.visibility_off,
-                            ),
-                            onPressed: () {
-                              setState(() {
-                                _showpass = !_showpass;
-                              });
-                            },
-                          ),
-                        ),
-                      ),
-                      const SizedBox(height: 28),
-                      // Confirm Password field
-                      TextFormField(
-                        controller: _confirmpass,
-                        obscureText: !_showconfirmpass,
-                        decoration: InputDecoration(
-                          labelText: 'CONFIRM PASSWORD',
-                          prefixIcon: const Icon(Icons.lock),
-                          filled: true,
-                          fillColor: Colors.white,
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(8),
-                            borderSide: const BorderSide(color: Colors.black, width: 2),
-                          ),
-                          suffixIcon: IconButton(
-                            icon: Icon(
-                              _showconfirmpass ? Icons.visibility : Icons.visibility_off,
-                            ),
-                            onPressed: () {
-                              setState(() {
-                                _showconfirmpass = !_showconfirmpass;
-                              });
-                            },
-                          ),
-                        ),
-                      ),
-                      const SizedBox(height: 55),
-                      // Sign up button
-                      ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.blue,
-                          padding: const EdgeInsets.symmetric(vertical: 15),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(8),
-                          ),
-                        ),
-                        onPressed: () {},
-                        child: const Text(
-                          'SIGN UP',
-                          style: TextStyle(
-                              fontSize: 24,
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold),
-                          textAlign: TextAlign.center,
-                        ),
-                      ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
               ),
             ],

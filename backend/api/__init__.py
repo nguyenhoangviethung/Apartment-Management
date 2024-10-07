c05061125198a8d501480c436c6c4508f99de436
dung1311 <109650915+dung1311@users.noreply.github.com>
2024-10-07 11:14:36 +0700

    update index route

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -27,8 +27,8 @@ def create_app(config_class=Config):
     swagger_bp = get_swaggerui_blueprint(app.config.get('SWAGGER_URL'), app.config.get('API_URL'), config={'app_name': 'auth api'})
     app.register_blueprint(swagger_bp, url_prefix= app.config.get('SWAGGER_URL'))                                       
 
-    @app.route('/test')
+    @app.route('/')
     def test():
-        return '<h1>THIS IS TEST FROM INIT</h1>'
+        return '<h1>THIS IS INDEX FROM INIT</h1>'
     
     return app

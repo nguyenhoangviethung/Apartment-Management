a34f9a3b5dbb68be55c6e6a44f25df770a1d9eee
LLRiver123 <thelake2004@gmail.com>
2024-10-26 20:55:06 +0700

    add minor api for displaying user info

--- backend/api/user/routes.py
+++ backend/api/user/routes.py
@@ -1,7 +1,10 @@
-from flask import redirect, url_for
+from flask import redirect, url_for, jsonify, request
 from api.user import user_bp
 from helpers import getIP
 from datetime import datetime, timedelta
+from api.middlewares import token_required, handle_exceptions
+from api.extensions import db
+from api.models.models import *
 
 @user_bp.route('/<int:household_id>/<int:amount>/pay')
 def pay(household_id, amount):
@@ -15,3 +18,25 @@ def pay(household_id, amount):
 
     return redirect(url_for('pay.payment', vnp_Amount=vnp_Amount, vnp_IpAddr=vnp_IpAddr, vnp_OrderInfo=vnp_OrderInfo, vnp_CreateDate=vnp_CreateDate, vnp_ExpireDate=vnp_ExpireDate))
     
+@user_bp.get('/info')
+@token_required
+@handle_exceptions
+def user_info(data):
+    user = Users.query.filter_by(user_id = data.get('user_id')).first()
+    info = {
+        'username' : user.username,
+        'user_role' : user.user_role,
+        'user_email': user.user_email,
+        'phone_number': user.phone_number
+    }
+    return jsonify({'info': info}), 200
+    
+# @user_bp.route('/update')   
+# @token_required
+# @handle_exceptions 
+# def update_info(data):
+#     user = Users.query.filter_by(user_id = data.get('user_id')).first()
+#     username = request.form.get('new_name')
+    
+#     user_email
+#     phone_number

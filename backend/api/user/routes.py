c1e1c7cc34c35902f2a4cd2a50c3857165c63cb2
dung1311 <109650915+dung1311@users.noreply.github.com>
2024-11-25 16:51:16 +0700

    add user/fees
    get fee information of resident's house. if user is not a resident, user dont have permission

--- backend/api/user/routes.py
+++ backend/api/user/routes.py
@@ -5,6 +5,7 @@ from datetime import datetime, timedelta
 from api.middlewares import token_required, handle_exceptions
 from api.extensions import db
 from api.models.models import *
+from api.models import fee_service
 
 @user_bp.route('/<int:household_id>/<int:amount>/pay')
 def pay(household_id, amount):
@@ -27,16 +28,42 @@ def user_info(data):
         'username' : user.username,
         'user_role' : user.user_role,
         'user_email': user.user_email,
-        'phone_number': user.phone_number
+        'phone_number': user.phone_number,
+        
     }
     return jsonify({'info': info}), 200
     
-# @user_bp.route('/update')   
-# @token_required
-# @handle_exceptions 
-# def update_info(data):
-#     user = Users.query.filter_by(user_id = data.get('user_id')).first()
-#     username = request.form.get('new_name')
+@user_bp.route('/update')   
+@token_required
+@handle_exceptions 
+def update_user_info(data):
+    user = Users.query.filter_by(user_id = data.get('user_id')).first()
     
-#     user_email
-#     phone_number
+    data = {
+        'username' : request.form.get('new_name'),
+        'user_email' : request.form.get('new_email'),
+        'phone_number' : request.form.get('new_number')
+    }
+    for field, value in data.items():
+        if value is not None:
+            setattr(user, field, value)
+            
+    db.session.commit()
+    return jsonify({'message': 'User info updated successfully!!!'}), 200        
+
+@user_bp.route('/fees')
+@token_required
+def fees(data):
+    user_id = data.get('user_id')
+    resident_id = db.session.query(Residents.resident_id).filter(Residents.user_id == user_id).scalar()
+    if not resident_id:
+        return jsonify({'message': 'you do not have permission'}), 403
+    
+    fee_info = fee_service.user_get_current_fee(resident_id)
+    result = {
+        'amount' : fee_info.amount,
+        'due_date' : fee_info.due_date,
+        'status' : fee_info.status,
+        'name_fee' : fee_info.description
+    }
+    return jsonify(result), 200

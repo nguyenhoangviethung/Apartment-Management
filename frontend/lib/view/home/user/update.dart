1887b4ee458fc51e239ba6b651f643f0178bccc6
minhnion <nguyenminh2k4pchy@gmail.com>
2024-12-01 16:01:59 +0700

    api user update

--- frontend/lib/view/home/user/update.dart
+++ frontend/lib/view/home/user/update.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:frontend/common/custom_date_picker.dart';
-import 'package:intl/intl.dart';
 import 'package:frontend/View/Home/main_home.dart';
+import 'package:frontend/common/show_dialog.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
 
 
 class Update extends StatefulWidget {
@@ -14,28 +15,48 @@ class Update extends StatefulWidget {
 class _UpdateState extends State<Update> {
   DateTime selectedDate = DateTime.now();
   String _dob = '';
+  TextEditingController name= TextEditingController();
+  TextEditingController email= TextEditingController();
+  TextEditingController phoneNumber= TextEditingController();
+  bool _isloading =false;
+  
 
-  void _showSuccessMessage(BuildContext context) {
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(
-        content: const Center(
-          child: Text(
-            'Update successful!',
-            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-            textAlign: TextAlign.center,
-          ),
-        ),
-        backgroundColor: Colors.green,
-        duration: const Duration(seconds: 2),
-        behavior: SnackBarBehavior.floating,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10),
-        ),
-        padding: const EdgeInsets.all(16),
-      ),
-    );
+  Future<void> userUpdate(String newName,String newEmail, String newNumber)async{
+    setState(() {
+      _isloading=true;
+    });
+    try{
+      SharedPreferences prefs= await SharedPreferences.getInstance();
+      String ?tokenlogin = prefs.getString('tokenlogin');
+      const String url='https://apartment-management-kjj9.onrender.com/user/update';
+      final response= await http.post(
+        Uri.parse(url),
+        headers: {
+          'Content-Type':'application/x-www-form-urlencoded',
+          'Authorization': 'Bearer $tokenlogin',
+        },
+        body: {
+          'new_name':newName,
+          'new_email':newEmail,
+          'new_number':newNumber
+        }
+      );
+      print(tokenlogin);
+      print(response.statusCode);
+      if(response.statusCode==200){
+        showinform(context, 'Success', 'User info updated successfully');
+      }else{
+        print('Error: ${response.statusCode}');
+        showinform(context, 'Failed', 'Try again');
+      }
+    }catch(e){
+      print('Error: $e');
+    }finally{
+      setState(() {
+        _isloading=false;
+      });
+    }
   }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -86,37 +107,36 @@ class _UpdateState extends State<Update> {
             const SizedBox(height: 20),
 
             // Các trường thông tin
-            _buildInfoField(label: 'Name', icon: Icons.person),
-            _buildInfoField(label: 'SDT', icon: Icons.phone),
-            _buildInfoField(label: 'Role', icon: Icons.work),
-            // _buildInfoField(label: 'Date of birth', icon: Icons.calendar_today),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                const SizedBox(width: 10),
-                const Text(
-                  'Date of birth: ',
-                  style: TextStyle(fontSize: 18),
-                ),
-                const SizedBox(width: 10),
-                Expanded(
-                  child: CustomDatePicker(
-                    initialDate: selectedDate,
-                    onDateSelected: (date) {
-                      setState(() {
-                        selectedDate = date; // Cập nhật ngày đã chọn
-                        _dob = DateFormat('yyyy-MM-dd').format(date);
-                      });
-                    },
-                    label: '',
-                  ),
-                ),
-              ],
-            ),
-            _buildInfoField(label: 'Age', icon: Icons.person_outline),
-            _buildInfoField(label: 'Avatar', icon: Icons.picture_in_picture),
-            _buildInfoField(label: 'ID Number', icon: Icons.credit_card),
-            _buildInfoField(label: 'Room', icon: Icons.room),
+            _buildInfoField(label: 'Name', icon: Icons.person,controller: name),
+            _buildInfoField(label: 'Email', icon: Icons.email,controller: email),
+            _buildInfoField(label: 'Phone number', icon: Icons.phone,controller: phoneNumber),
+            // Row(
+            //   mainAxisAlignment: MainAxisAlignment.start,
+            //   children: [
+            //     const SizedBox(width: 10),
+            //     const Text(
+            //       'Date of birth: ',
+            //       style: TextStyle(fontSize: 18),
+            //     ),
+            //     const SizedBox(width: 10),
+            //     Expanded(
+            //       child: CustomDatePicker(
+            //         initialDate: selectedDate,
+            //         onDateSelected: (date) {
+            //           setState(() {
+            //             selectedDate = date; // Cập nhật ngày đã chọn
+            //             _dob = DateFormat('yyyy-MM-dd').format(date);
+            //           });
+            //         },
+            //         label: '',
+            //       ),
+            //     ),
+            //   ],
+            // ),
+            // _buildInfoField(label: 'Age', icon: Icons.person_outline),
+            // _buildInfoField(label: 'Avatar', icon: Icons.picture_in_picture),
+            // _buildInfoField(label: 'ID Number', icon: Icons.credit_card),
+            // _buildInfoField(label: 'Room', icon: Icons.room),
 
             const SizedBox(height: 20),
 
@@ -124,11 +144,7 @@ class _UpdateState extends State<Update> {
             Center(
               child: ElevatedButton(
                 onPressed: () async {
-                  _showSuccessMessage(context);
-
-                  // Đợi SnackBar hiển thị xong rồi mới quay lại màn hình User
-                  await Future.delayed(const Duration(seconds: 2));
-                  Navigator.pop(context);
+                  userUpdate(name.text.trim(), email.text.trim(), phoneNumber.text.trim());
                 },
                 style: ElevatedButton.styleFrom(
                   padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 12),
@@ -138,7 +154,8 @@ class _UpdateState extends State<Update> {
                     borderRadius: BorderRadius.circular(8),
                   ),
                 ),
-                child: const Text(
+                child: _isloading? const CircularProgressIndicator(color: Colors.white,):
+                const Text(
                   'Update',
                   style: TextStyle(
                     fontSize: 16,
@@ -156,10 +173,12 @@ class _UpdateState extends State<Update> {
   Widget _buildInfoField({
     required String label,
     required IconData icon,
+    required TextEditingController controller,
   }) {
     return Padding(
       padding: const EdgeInsets.only(bottom: 16),
       child: TextField(
+        controller: controller,
         decoration: InputDecoration(
           labelText: label,
           prefixIcon: Icon(icon),

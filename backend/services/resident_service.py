cb2b81cba8b003aba2ade23eb7284a3078000172
nguyenhoangviethung <nguyenhoangviethung@gmail.com>
2024-12-10 23:05:22 +0700

    update resident_service

--- backend/services/resident_service.py
+++ backend/services/resident_service.py
@@ -5,78 +5,39 @@ from datetime import datetime
 from api.extensions import db
 import logging
 from helpers import validate_date
+from api.middlewares import handle_exceptions
 
 class ResidentService:
     def __init__(self):
         self.logger = logging.getLogger(__name__)
-
-    def create_resident(self, resident_data): # nho them kiem tra da ton tai resident_id
-        try:
-            existing_resident = db.query(Residents).filter_by(resident_id=resident_data.get('resident_id')).first()
-            if existing_resident:
-                raise Exception({"error": "Resident with this resident_id already exists."})
-            dob = resident_data.get('date_of_birth')
-            if dob:
-                try:
-                    date_of_birth = datetime.strptime(dob, '%Y-%m-%d').date()
-                except ValueError:
-                    raise Exception({"error": "Invalid date format for date_of_birth. Use YYYY-MM-DD."})
-            else:
-                date_of_birth = None
-            if not db.query(Households).filter(Households.household_id == resident_data.get('household_id')).first():
-                raise Exception(f'Household_id not found') 
-            new_resident = Residents(
-                resident_id = resident_data.get('resident_id'),
-                user_id = resident_data.get('user_id'),
-                household_id = resident_data.get('household_id'),
-                resident_name = resident_data.get('resident_name'),
-                date_of_birth=date_of_birth,
-                id_number = resident_data.get('id_number'),
-                status = resident_data.get('status'),
-                phone_number = resident_data.get('phone_number')
-            )
-            
-            db.add(new_resident)
-            db.commit()
-            db.refresh(new_resident)
-            return new_resident
-        except Exception as e:
-            raise Exception(f'{e}')
-    
-    
-    def add_resident(self, data):
-        required_fields = ["resident_id", "household_id", "resident_name","status"]
-        if not all(field in data for field in required_fields):
-            return {'error': 'Missing required fields'}, 400
-        
-        existing_resident = db.session.query(Residents).filter_by(resident_id=data.get('resident_id')).first()
+    @handle_exceptions
+    def add_resident(self, resident_data): # nho them kiem tra da ton tai resident_id
+        existing_resident = db.session.query(Residents).filter_by(resident_id=resident_data.get('resident_id')).first()
         if existing_resident:
-            return ({"error": "Resident with this resident_id already exists."}), 404
-        
-        dob = data.get('date_of_birth')
-        if dob:
-            date_of_birth = validate_date(data.get('date_of_birth')) or None
-        else:
-            date_of_birth = None
-
-        if not db.session.query(Households).filter(Households.household_id == data.get('household_id')).first():
-                return (f'Household_id not found'), 404    
-
+            return ("error: Resident with this resident_id already exists.") , 400
+        dob = resident_data.get('date_of_birth')
+        try:
+            date_of_birth = validate_date(dob)
+        except:
+            return ("error: Invalid date") , 400
+        if not db.session.query(Households).filter(Households.household_id == resident_data.get('household_id')).first():
+            return ('error: Household_id not found') , 400
         new_resident = Residents(
-            resident_id = data.get('resident_id'),
-            user_id = data.get('user_id'),
-            household_id = data.get('household_id'),
-            resident_name = data.get('resident_name'),
+            resident_id = resident_data.get('resident_id'),
+            user_id = resident_data.get('user_id'),
+            household_id = resident_data.get('household_id'),
+            resident_name = resident_data.get('resident_name'),
             date_of_birth=date_of_birth,
-            id_number = data.get('id_number'),
-            status = data.get('status'),
-            phone_number = data.get('phone_number'),
-            household_registration = data.get('household_registration')
+            id_number = resident_data.get('id_number'),
+            status = resident_data.get('status'),
+            phone_number = resident_data.get('phone_number'),
+            household_registration = resident_data.get('household_registration')
         )
-
+        
         db.session.add(new_resident)
         db.session.commit()
-        return ("messages: add resident succesfull"), 200
+        db.session.refresh(new_resident)
+        return ('message: add resident successfully'), 201
 
     def get_resident(self, resident_id):
         return db.query(Residents).filter(Residents.resident_id == resident_id).first()
